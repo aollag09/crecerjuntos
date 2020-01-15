b31a4ef590fd47aaef75a666bdeb241d51fabad8
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-01-15 16:07:23 -0500

    Add test on Sections

---
+++ app/src/test/java/com/crecerjuntos/model/data/SectionTest.java
@@ -0,0 +1,23 @@
+package com.crecerjuntos.model.data;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SectionTest {
+
+    @Test
+    public void test_equals(){
+        Section s1 = new Section( "id1", "sectio1");
+        Section s2 = new Section( "id1", "section2");
+        Section s3 = new Section( "id3", "section3");
+        Assert.assertEquals(s1, s2);
+        Assert.assertNotEquals(s1, s3);
+        Assert.assertNotEquals(s2, s3);
+    }
+
+    @Test
+    public void test_default(){
+        Section sd = new Section("id");
+        Assert.assertEquals(sd.getName(), Section.DEFAULT_NAME);
+    }
+}
