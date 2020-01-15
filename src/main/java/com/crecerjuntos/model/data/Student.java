0dc12449fcc84e7dfa13cb1ef36cb9a2136da0bd
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-01-15 15:28:12 -0500

    Create student data model class

---
+++ src/main/java/com/crecerjuntos/model/data/Student.java
@@ -0,0 +1,72 @@
+package com.crecerjuntos.model.data;
+
+import com.google.common.base.Objects;
+
+public class Student {
+
+    public static final String DEFAULT_NAME = "Anonymous";
+    public static final String DEFAULT_SECTION = "No Section";
+
+    /** Private generated id */
+    private final String id;
+
+    /** Public unique name */
+    private final String name;
+
+    /** Id of the associated section */
+    private final String section;
+
+    public Student( final String id, final String name, final String section ) {
+        this.id = id;
+        this.name = name;
+        this.section = section;
+    }
+
+    public Student( final String id, final String name) {
+        this.id = id;
+        this.name = name;
+        this.section = DEFAULT_SECTION;
+    }
+
+    public Student( final String id ){
+        this.id = id;
+        this.name = DEFAULT_NAME;
+        this.section = DEFAULT_SECTION;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equal(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", section='" + section + '\'' +
+                '}';
+    }
+}
+
+
---
+++ src/test/java/com/crecerjuntos/model/data/StudentTest.java
@@ -0,0 +1,28 @@
+package com.crecerjuntos.model.data;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StudentTest {
+
+    @Test
+    public void test_equals() {
+        Student s1 = new Student("id", "name1", "c1");
+        Student s2 = new Student("id", "name2", "c2");
+        Student s3 = new Student("id3", "name1", "c1");
+        Assert.assertEquals(s1, s2);
+        Assert.assertNotEquals(s1, s3);
+        Assert.assertNotEquals(s2, s3);
+    }
+
+    @Test
+    public void test_default_values() {
+        Student s1 = new Student("id1");
+        Assert.assertEquals(s1.getName(), Student.DEFAULT_NAME);
+        Assert.assertEquals(s1.getSection(), Student.DEFAULT_SECTION);
+
+        Student s2 = new Student("id1", "toto");
+        Assert.assertEquals(s2.getSection(), Student.DEFAULT_SECTION);
+    }
+
+}
