ab0700a85bc31622dc9a7bfcb7fb2e1652effd9d
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-01-15 15:48:33 -0500

    Upgrade gradle 6.0.1

---
+++ src/test/java/com/crecerjuntos/model/data/SectionTest.java
@@ -0,0 +1,15 @@
+package com.crecerjuntos.model.data;
+
+import org.junit.Test;
+
+public class SectionTest {
+
+    @Test
+    public void test_equals(){
+
+    }
+
+    public void test_default_values(){
+
+    }
+}
