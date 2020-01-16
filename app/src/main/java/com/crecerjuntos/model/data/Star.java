ab59b58713a8d54026cdfead600d8095c831affe
MowMow <amaury.ollagnier@gmail.com>
2020-01-16 18:31:34 +0000

    Create star

---
+++ app/src/main/java/com/crecerjuntos/model/data/Star.java
@@ -0,0 +1,20 @@
+package com.crecerjuntos.model.data;
+
+import com.google.common.base.Objects;
+
+public class Star {
+
+    /** Unique, private & generated id */
+    private final String id;
+
+    /** Identifier of the student */
+    private final String studentId;
+
+    /** Name of the exercice */
+    private final String exercice;
+
+    /** Number of Stars, between 0 and 5 */
+    private final int stars;
+
+
+}
