9d992fb43df977a0de41c9b1d04cddaa74a0d07f
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-01-16 19:53:53 -0500

    Add greety server

---
+++ front/src/main/java/com/crecerjuntos/front/HomePage.java
@@ -0,0 +1,11 @@
+package com.crecerjuntos.front;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+public class HomePage extends UI {
+    @Override
+    protected void init(VaadinRequest request) {
+
+    }
+}
