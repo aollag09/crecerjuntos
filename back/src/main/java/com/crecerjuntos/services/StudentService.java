f90e127955af1c8aea16bc375f160b2cd815fea3
Isabelle Robin <isabelle.s.robin@gmail.com>
2020-01-19 12:03:12 -0500

    Add Authoring access implementation to student service

--- back/src/main/java/com/crecerjuntos/services/StudentService.java
+++ back/src/main/java/com/crecerjuntos/services/StudentService.java
@@ -4,6 +4,7 @@ import com.crecerjuntos.infrastructure.AchievementRepository;
 import com.crecerjuntos.infrastructure.StudentRepository;
 import com.crecerjuntos.model.Achievement;
 import com.crecerjuntos.model.Section;
+import com.crecerjuntos.model.Star;
 import com.crecerjuntos.model.Student;
 import com.crecerjuntos.model.base.IAuthoringServices;
 import com.crecerjuntos.model.base.IStudentAccess;
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class StudentService implements IStudentAccess {
+public class StudentService implements IStudentAccess, IAuthoringServices {
 
     @Autowired
     StudentRepository studentRepository;
@@ -44,11 +45,43 @@ public class StudentService implements IStudentAccess {
         return achievement.getStudent();
     }
 
-    public Student add(Student student){
-        return studentRepository.save(student);
+    @Override
+    public void add(Student student){
+        studentRepository.save(student);
     }
 
+    @Override
     public void remove(Student student){
         studentRepository.delete(student);
+    }
+
+    @Override
+    public void add(Section section) {
+        throw new IllegalArgumentException("Cannot add section with StudentService");
+    }
+
+    @Override
+    public void remove(Section section) {
+        throw new IllegalArgumentException("Cannot remove section with StudentService");
+    }
+
+    @Override
+    public void add(Achievement achievement) {
+        throw new IllegalArgumentException("Cannot add achievement with StudentService");
+    }
+
+    @Override
+    public void remove(Achievement achievement) {
+        throw new IllegalArgumentException("Cannot remove achievement with StudentService");
+    }
+
+    @Override
+    public void add(Star star) {
+        throw new IllegalArgumentException("Cannot add star with StudentService");
+    }
+
+    @Override
+    public void remove(Star star) {
+        throw new IllegalArgumentException("Cannot remove star with StudentService");
     }
 }
