58ee0c38f2e6b2818911c840124430440660b5b4
Isabelle Robin <isabelle.s.robin@gmail.com>
2020-01-19 15:31:55 -0500

    Remove section initialization

--- back/src/main/java/com/crecerjuntos/CSCoursesApp.java
+++ back/src/main/java/com/crecerjuntos/CSCoursesApp.java
@@ -2,44 +2,28 @@ package com.crecerjuntos;
 
 import com.crecerjuntos.infrastructure.AchievementRepository;
 import com.crecerjuntos.infrastructure.SectionRepository;
-import com.crecerjuntos.infrastructure.StudentRepository;
-import com.crecerjuntos.model.Section;
+import com.crecerjuntos.services.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CSCoursesApp implements CommandLineRunner {
+public class CSCoursesApp {
 
-  private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  @Autowired StudentRepository studentRepository;
+    @Autowired
+    StudentService studentService;
 
-  @Autowired SectionRepository sectionRepository;
+    @Autowired
+    SectionRepository sectionRepository;
 
-  @Autowired AchievementRepository achievementRepository;
+    @Autowired
+    AchievementRepository achievementRepository;
 
-  public static void main(String[] args) {
-    SpringApplication.run(CSCoursesApp.class, args);
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
-    logger.info(
-        "Registering aceleracion-> {}", sectionRepository.save(new Section(1L, "Aceleracion")));
-    logger.info("Registering segundo -> {}", sectionRepository.save(new Section(2L, "Segundo")));
-    logger.info("Registering tercero -> {}", sectionRepository.save(new Section(3L, "Tercero")));
-    logger.info("Registering quarto -> {}", sectionRepository.save(new Section(4L, "Quarto")));
-    logger.info("Registering quinto -> {}", sectionRepository.save(new Section(5L, "Quinto")));
-    logger.info("Registering sexto -> {}", sectionRepository.save(new Section(6L, "Sexto")));
-    logger.info("Registering septimo -> {}", sectionRepository.save(new Section(7L, "Septimo")));
-    logger.info("Registering octavo -> {}", sectionRepository.save(new Section(8L, "Octavo")));
-    logger.info("Registering noveno -> {}", sectionRepository.save(new Section(9L, "Noveno")));
-    logger.info(
-        "Registering proceso basico-> {}",
-        sectionRepository.save(new Section(10L, "Proceso Basico")));
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(CSCoursesApp.class, args);
+    }
 }
