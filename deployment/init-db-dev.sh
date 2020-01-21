e9664cdb325d80b9fc0efb51e466a35bd31b76fc
Isabelle Robin <isabelle.s.robin@gmail.com>
2020-01-20 22:18:40 -0500

    Add docker volume local mapping and remove init sql script

--- deployment/init-db-dev.sh
+++ deployment/init-db-dev.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-
 docker pull postgres:9.5
-docker run -e POSTGRES_PASSWORD=docker -d -p 5432:5432 postgres:9.5
+docker run -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:9.5
 sleep 4
-psql -h localhost -U postgres -d postgres -f ../database/init.sql
