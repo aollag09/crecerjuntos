50e9284ffc4b9429db74ec1e818b9612bb0f3614
Isabelle Robin <isabelle.s.robin@gmail.com>
2020-01-23 14:46:01 -0500

    Use psql client inside docker

--- deployment/init-db-dev.sh
+++ deployment/init-db-dev.sh
@@ -6,9 +6,9 @@ if lsof -Pi :5432 -sTCP:LISTEN -t >/dev/null ; then
 else
     echo "Launching Postgres docker"
     docker pull postgres:9.5
-    docker run -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:9.5
+    docker run --name crecer_juntos -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:9.5
     sleep 4
 fi
 
 # Check database was created and if not, create it
-psql -h localhost -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'cs_courses'" | grep -q 1 || psql -h localhost -U postgres -d postgres -f ../database/init.sql
+docker exec -it crecer_juntos psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'cs_courses'" | grep -q 1 || docker exec -it crecer_juntos psql -U postgres -f ../database/init.sql
