41022d0b4dfa2e024e97c8f11184f1bbf6a60bf8
Isabelle Robin <isabelle.s.robin@gmail.com>
2020-01-22 19:24:23 -0500

    Fix database initiation script with checks of ports and database creation

--- deployment/init-db-dev.sh
+++ deployment/init-db-dev.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
-docker pull postgres:9.5
-docker run -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:9.5
-sleep 4
+
+# Launch database container
+if lsof -Pi :5432 -sTCP:LISTEN -t >/dev/null ; then
+    echo "Postgres default port already taken. Assuming database was launched"
+else
+    echo "Launching Postgres docker"
+    docker pull postgres:9.5
+    docker run -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:9.5
+    sleep 4
+fi
+
+# Check database was created and if not, create it
+psql -h localhost -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'cs_courses'" | grep -q 1 || psql -h localhost -U postgres -d postgres -f ../database/init.sql
