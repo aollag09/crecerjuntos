0adf636d5cec53e0ee91816e2887cb7c284d07ae
Isabelle Robin <isabelle.s.robin@gmail.com>
2020-02-03 16:37:44 -0500

    Fix test

--- back/src/test/java/com/crecerjuntos/services/DatabaseErrorManagementTest.java
+++ back/src/test/java/com/crecerjuntos/services/DatabaseErrorManagementTest.java
@@ -5,29 +5,38 @@ import com.crecerjuntos.model.Student;
 import com.crecerjuntos.model.exception.DataBaseException;
 import org.junit.Test;
 
+import javax.xml.crypto.Data;
 import java.util.Calendar;
 
 public class DatabaseErrorManagementTest {
 
-  // @Test(expected = DataBaseException.class)
+  @Test(expected = DataBaseException.class)
   public void test_delete_non_existing() throws DataBaseException {
-    TestServices.authoringServices.remove(new Student(TestServices.random.nextLong()));
+    Student student1 = new Student("Unexisting");
+    TestServices.authoringServices.add(student1);
+    TestServices.authoringServices.remove(student1);
+    TestServices.authoringServices.remove(student1);
   }
 
-  // @Test(expected = DataBaseException.class)
+  @Test(expected =  DataBaseException.class)
+  public void test_delete_not_added() throws DataBaseException {
+    Student not_added = new Student ("Peter");
+    TestServices.authoringServices.remove(not_added);
+  }
+
+  @Test(expected = DataBaseException.class)
   public void test_create_existing_student() throws Exception {
     Student test = TestServices.generateTestStudent();
     Student sameStudent =
         new Student(
-            test.getId(), test.getName(), test.getMail(), test.getPassword(), test.getSection());
+            test.getName(), test.getMail(), test.getPassword(), test.getSection());
     TestServices.authoringServices.add(sameStudent);
   }
 
-  //@Test(expected = DataBaseException.class)
+  @Test(expected = DataBaseException.class)
   public void test_start_non_existing_user() throws Exception {
     TestServices.authoringServices.add(
         new Achievement(
-            TestServices.random.nextLong(),
             new Student(),
             "session",
             new java.sql.Date(Calendar.getInstance().getTime().getTime()),
@@ -37,11 +46,11 @@ public class DatabaseErrorManagementTest {
             0));
   }
 
-  //@Test
+  @Test
   public void test_transaction_closed_on_failed_student() throws Exception {
     // Generate error
     try {
-      TestServices.authoringServices.remove(new Student(TestServices.random.nextLong()));
+      TestServices.authoringServices.remove(new Student());
     } catch (DataBaseException e) {
       // That is ok
     }
@@ -50,13 +59,12 @@ public class DatabaseErrorManagementTest {
     TestServices.generateTestStudent();
   }
 
-  //@Test
+  @Test
   public void test_transaction_closed_on_failed_achievement() throws Exception {
     // Generate error
     try {
       TestServices.authoringServices.add(
               new Achievement(
-                      TestServices.random.nextLong(),
                       new Student(),
                       "session",
                       new java.sql.Date(Calendar.getInstance().getTime().getTime()),
