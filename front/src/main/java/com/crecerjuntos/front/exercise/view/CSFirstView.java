8c27ad875244a847c45b11fdfa9856403e9b066e
profesor <amaury.ollagnier@gmail.com>
2020-02-10 12:56:08 -0500

    Update CSFirst Link
    (cherry picked from commit c38d1f3b8b172c8c773378cf21ae783f31f722f8)

--- front/src/main/java/com/crecerjuntos/front/exercise/view/CSFirstView.java
+++ front/src/main/java/com/crecerjuntos/front/exercise/view/CSFirstView.java
@@ -40,7 +40,7 @@ public class CSFirstView extends AbstractExerciseView {
     go.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
     go.addClickListener(
         event -> {
-          UI.getCurrent().getPage().setLocation("https://csfirst.withgoogle.com/login");
+          UI.getCurrent().getPage().setLocation("https://scratch.mit.edu/projects/340739176/editor");
         });
     instructions.add(go);
 
